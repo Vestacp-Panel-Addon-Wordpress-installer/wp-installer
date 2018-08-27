f3be62d7dd6662ef7f7a899d3f94a09f64155c9e
sam watttoo <sam927752@gmail.com>
2018-08-27 01:33:37 -0700

    Update vwpinstall.sh

--- vwpinstall.sh
+++ vwpinstall.sh
@@ -18,9 +18,7 @@ chmod 644 /usr/local/vesta/web/list/wp/index.php
 chmod 644 /usr/local/vesta/web/list/wp/api.php
 # Add the link to the panel.html file 
 if grep -q 'WP' /usr/local/vesta/web/templates/admin/panel.html; then
-		echo 'Already there.'
-	else 
-sed -i '/<div class="l-menu clearfix noselect">/a <div class="l-menu__item <?php if($TAB == "WP" ) echo "l-menu__item--active"; ?>"><a href="/list/wp/" target="_blank"><?=__("WP Install")?></a></div>' /usr/local/vesta/web/templates/admin/panel.html;
+		echo 'Already there.' else  sed -i '/<div class="l-menu clearfix noselect">/a <div class="l-menu__item <?php if($TAB == "WP" ) echo "l-menu__item--active"; ?>"><a href="/list/wp/" target="_blank"><?=__("WP Install")?></a></div>' /usr/local/vesta/web/templates/admin/panel.html;
 fi
 echo "Done! Check VestaCP!" 
 }
